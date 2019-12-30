4b17009cdd5cd4d7e0e438f57bd13afa6f32ffb7
Jake Cordero <jakemcordero@gmail.com>
2019-12-29 18:18:19 -0800

    Display selections recursively

--- src/Summary.js
+++ src/Summary.js
@@ -33,11 +33,9 @@ class ForceSummary extends Component {
                 <h2>{this.props.force.getAttribute('name')}&nbsp;({this.props.force.getAttribute('catalogueName')})</h2>
                 <ul className="ForceSummary-categories">
                     {Object.entries(this.selectionsByCategory()).map(([name, selections]) =>
-                        <li key={name}>{name}
-                            <ul>
-                                {selections.map((s) =>
-                                    <UnitSummary roster={this.props.roster} selection={s} key={s.id} />)}
-                            </ul>
+                        <li key={name}>
+                            {name}
+                            <SelectionsSummary roster={this.props.roster} selections={selections} />
                         </li>
                     )}
                 </ul>
@@ -46,20 +44,43 @@ class ForceSummary extends Component {
     }
 }
 
-class UnitSummary extends Component {
+function selectionHasUnit(r, s) {
+    return r.xpath(".//profile[@typeName='Unit']", s).length
+}
+
+class SelectionsSummary extends Component {
+
     render() {
-        const models = this.props.roster.xpath(
-            "./selections/selection/profiles/profile[@typeName='Unit']",
-            this.props.selection,
-        ).map(profile => profile.parentNode.parentNode);
+        const sorted = this.props.selections.sort((x, y) => {
+            return selectionHasUnit(this.props.roster, x) - selectionHasUnit(this.props.roster, y);
+        });
+
         return (
-            <li className="UnitSummary">
-                {this.props.selection.getAttribute('name')}
-                <ul>
-                    {models.map(model => <li key={model.id}>
-                        {model.getAttribute('number')}x {model.getAttribute('name')}
-                    </li>)}
-                </ul>
+            <ul>{sorted.map(s =>
+                <SelectionSummary roster={this.props.roster} selection={s} key={s.id} />
+            )}</ul>
+        );
+
+    }
+}
+
+
+class SelectionSummary extends Component {
+
+    render() {
+        const selections = this.props.roster.xpath('./selections/selection', this.props.selection);
+        const number = parseInt(this.props.selection.getAttribute('number'), 10);
+        const name = this.props.selection.getAttribute('name');
+
+        const number_and_name = (number > 1 ? number + 'x ' : '') + name;
+
+        return (
+            <li className="SelectionSummary">
+                {selectionHasUnit(this.props.roster, this.props.selection)
+                    ? <b>{number_and_name}</b>
+                    : <em>{number_and_name}</em>
+                }
+                <SelectionsSummary roster={this.props.roster} selections={selections} />
             </li>
         )
     }
