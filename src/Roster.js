ee91228fd77a2e5e706faf335e4785d63cb5bdb6
Jake Cordero <jakemcordero@gmail.com>
2019-12-29 20:50:01 -0800

    Add tables with weapons and stats

--- src/Roster.js
+++ src/Roster.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './Roster.css';
 
 import Summary from './Summary.js';
+import Stats from './Stats.js';
 
 class Roster extends Component {
     xpath(path, subsearch) {
@@ -29,7 +30,26 @@ class Roster extends Component {
             <div className="Roster">
                 <h1>{this.at_xpath('/roster').getAttribute('name')}</h1>
                 <Summary roster={this} />
-                {/* TODO: cheatsheet goes here */}
+                <h1>Tables</h1>
+                <Stats roster={this} type="Unit" />
+                <Stats roster={this} type="Weapon" sort={(p1, p2) => {
+                    const type1 = this.at_xpath(".//characteristic[@name='Type']", p1).textContent;
+                    const type2 = this.at_xpath(".//characteristic[@name='Type']", p2).textContent;
+
+                    if ((type1 === 'Melee' && type2 === 'Melee') || (type1 !== 'Melee' && type2 !== 'Melee')) {
+                        const name1 = p1.getAttribute('name');
+                        const name2 = p2.getAttribute('name');
+                        if (name1 < name2) { return -1; }
+                        if (name1 > name2) { return 1; }
+                        return 0;
+                    } else if (type1 === 'Melee') {
+                        return 1;
+                    } else if (type2 === 'Melee') {
+                        return -1;
+                    }
+
+                    throw new Error('Should be unreachable, but what do I know?');
+                }}/>
             </div>
         );
     }
---
+++ src/Stats.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import './Stats.css';
+
+class Stats extends Component {
+    render() {
+        const profiles = this.props.roster.xpath("//profile[@typeName='" + this.props.type + "']");
+        if (this.props.sort != null) {
+            profiles.sort(this.props.sort);
+        }
+        const statlines = profiles.reduce((acc, p) => {
+            const name = p.getAttribute('name');
+            const statline = this.props.roster.xpath(".//characteristic", p).reduce((sts, ch) => {
+                sts[ch.getAttribute('name')] = ch.textContent;
+                return sts;
+            }, {});
+
+            acc[name] = statline;
+            return acc;
+        }, {});
+
+        return (
+            <div className="Stats">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Model</th>
+                            {Object.keys(Object.values(statlines)[0]).map(k => <th key={k}>{k}</th>)}
+                        </tr>
+                    </thead>
+                    <tbody >
+                        {Object.entries(statlines).map(([name, st]) =>
+                            <tr key={name}>
+                                <td>{name}</td>
+                                {Object.entries(st).map( ([k,v])=> <td key={k}>{v}</td>)}
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
+
+export default Stats;
